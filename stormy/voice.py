7060e66200669c2cba244d82632700d0a53250fd
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-03-18 23:21:39 +0200

    Update voice.py

--- stormy/voice.py
+++ stormy/voice.py
@@ -1,10 +1,10 @@
 import pyttsx3
 
 engine = pyttsx3.init()
-engine.setProperty('rate', 170)   # Speed
+engine.setProperty('rate', 170)
 engine.setProperty('volume', 0.9)
 
 def speak(text: str):
-    print(f"Stormy (voice): {text}")
+    print(f"🌩️ Stormy: {text}")
     engine.say(text)
     engine.runAndWait()
